92bd319990c5c624d188ea8628b26f08e9da781a
AplineHunter <nagisakaworuseventeen@gmail.com>
2024-11-21 23:25:48 +0900

    パディング変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import GameBoard from '@/components/GameBoard';
  */
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen flex-col items-center justify-between p-4'>
       {/* ゲームボードコンポーネントをレンダリング */}
       <GameBoard />
     </main>
